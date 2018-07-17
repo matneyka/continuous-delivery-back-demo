340ffada815746ff7e2dc34342b11e21470f6861
wsingleton <wezley.singleton@gmail.com>
2018-07-16 23:15:21 -0400

    initialize repo

---
+++ src/main/java/com/revature/servlets/TestServlet.java
@@ -0,0 +1,25 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1062233248842877387L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.write("Application successfully deployed!");
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 
+	}
+}
